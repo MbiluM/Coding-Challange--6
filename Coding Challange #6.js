14d3f9aef90960238d1b2773a5f2f2c38b3c3add
MbiluM <mabilu@usf.edu>
2025-02-08 16:32:35 -0500

    Task 2 - Sales Tax Computation

--- Coding Challange #6.js
+++ Coding Challange #6.js
@@ -1,6 +1,12 @@
 // Task 1 - Business Profit Calculation
 const calculateProfit = (costPrice, sellingPrice, unitsSold) => {
-    return ((sellingPrice - costPrice) * unitsSold); // Profit Formula
+    return ((sellingPrice - costPrice) * unitsSold); //Profit Formula
 }
-console.log(calculateProfit(20, 30, 100)) // Expected Output: 1000
-console.log(calculateProfit(50, 70, 200)) // Expected Output: 4000
+console.log(calculateProfit(20, 30, 100)) //Expected Output: 1000
+console.log(calculateProfit(50, 70, 200)) //Expected Output: 4000
+
+// Task 2 - Sales Tax Computation 
+const calculateSalesTax = (amount, taxRate) => amount * taxRate; //Sales Tax Formula
+
+console.log(calculateSalesTax(100, 0.07)) //Output: 7
+console.log(calculateSalesTax(500, 0.1)) //Output: 50
